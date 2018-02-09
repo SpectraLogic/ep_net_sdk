8eb501a69759ceadef478b292c91b944dbdcd6b4
Sharon Shabtai <sharons@spectralogic.com>
2018-02-09 11:17:19 -0700

    remove version from test assemblyInfo to reduce nosie in PRs

--- SpectraLogic.EscapePodClient.Integration.Test/Properties/AssemblyInfo.cs
+++ SpectraLogic.EscapePodClient.Integration.Test/Properties/AssemblyInfo.cs
@@ -20,16 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5ee70381-47e5-44d6-b627-daa6b62731bf")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.115")]
-[assembly: AssemblyFileVersion("1.0.0.115")]
