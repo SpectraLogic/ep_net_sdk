4f5f14e264bf9577f279498ed2176e8941bb341a
Sharon Shabtai <sharons@spectralogic.com>
2019-10-30 12:11:26 -0600

    Fix conflics

--- SpectraLogic.SpectraRioBrokerClient/Properties/AssemblyInfo.cs
+++ SpectraLogic.SpectraRioBrokerClient/Properties/AssemblyInfo.cs
@@ -47,7 +47,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.674")]
-[assembly: AssemblyFileVersion("1.0.0.674")]
+[assembly: AssemblyVersion("1.0.0.676")]
+[assembly: AssemblyFileVersion("1.0.0.676")]
+
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 [assembly: InternalsVisibleTo("SpectraLogic.SpectraRioBrokerClient.Test")]
