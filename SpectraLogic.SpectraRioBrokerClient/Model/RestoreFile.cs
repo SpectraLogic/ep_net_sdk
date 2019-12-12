a328e83c623045d1555bc864715d45b541511368
Sharon Shabtai <sharons@spectralogic.com>
2019-12-12 14:29:31 -0700

    Add new TimeCodeRange converter

--- SpectraLogic.SpectraRioBrokerClient/Model/RestoreFile.cs
+++ SpectraLogic.SpectraRioBrokerClient/Model/RestoreFile.cs
@@ -13,13 +13,13 @@
  * ****************************************************************************
  */
 
-using System;
 using Newtonsoft.Json;
+using SpectraLogic.SpectraRioBrokerClient.Utils.JsonConverters;
+using System;
 
 namespace SpectraLogic.SpectraRioBrokerClient.Model
 {
     /// <summary>
-    ///
     /// </summary>
     public class RestoreFile
     {
@@ -40,7 +40,8 @@ namespace SpectraLogic.SpectraRioBrokerClient.Model
         /// The time code range
         /// </summary>
         [JsonProperty(PropertyName = "timeCodeRange", NullValueHandling = NullValueHandling.Ignore)]
-        public string TimeCodeRange;
+        [JsonConverter(typeof(TimeCodeRangeConverter))]
+        public TimeCodeRange TimeCodeRange;
 
         /// <summary>
         /// The URI
@@ -80,12 +81,16 @@ namespace SpectraLogic.SpectraRioBrokerClient.Model
         /// </summary>
         /// <param name="name">The name.</param>
         /// <param name="uri">The destination.</param>
-        /// <param name="timeCodeRange">Beginning and ending frame of the desired clip separated by '-'. Format: hh:mm:ss:ff for non-drop frame rates and hh:mm:ss;ff for drop frame rates. Example: 01:00:00;12-01:00:10;13 for drop frame rate.</param>
+        /// <param name="timeCodeRange">
+        /// Beginning and ending frame of the desired clip separated by '-'. Format: hh:mm:ss:ff for
+        /// non-drop frame rates and hh:mm:ss;ff for drop frame rates. Example:
+        /// 01:00:00;12-01:00:10;13 for drop frame rate.
+        /// </param>
         public RestoreFile(string name, Uri uri, TimeCodeRange timeCodeRange)
         {
             Name = name;
             Uri = uri.ToString();
-            TimeCodeRange = timeCodeRange.ToString();
+            TimeCodeRange = timeCodeRange;
         }
 
         #endregion Constructors
